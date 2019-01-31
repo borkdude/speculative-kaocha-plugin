91169daba6c726c6cb5af2763cf81e90c0a99971
Michiel Borkent <michielborkent@gmail.com>
2019-01-31 10:27:51 +0100

    Refactor test-suite? function, see #1

--- src/speculative_kaocha/plugin.clj
+++ src/speculative_kaocha/plugin.clj
@@ -2,23 +2,25 @@
   (:require [kaocha.plugin :refer [defplugin]]
             [speculative.instrument :refer [instrument unstrument]]))
 
-(defn test-suite? [test]
-  (contains? #{:kaocha.type/clojure.test
-               :kaocha.type/cucumber
-               :kaocha.type/cljs}
-             (:kaocha.testable/type test)))
+(defn test-suite?
+  "TODO: this function can be replaced using suite? from this commit
+  https://github.com/lambdaisland/kaocha/commit/76460da1ae702af29f7b9c402211368b1fa9b7aa
+  once it is released."
+  [test test-plan]
+  (contains?
+   (into #{}
+         (map :kaocha.testable/id)
+         (:kaocha.test-plan/tests test-plan))
+   (:kaocha.testable/id test)))
 
 (defplugin ::instrument
   (pre-test [test test-plan]
-    (let [suite? (into #{}
-                       (map :kaocha.testable/id)
-                       (:kaocha.test-plan/tests test-plan))]
-      (when (and (suite? (:kaocha.testable/id test))
-                 (not (::no-instrument test)))
-        (instrument)))
+    (when (and (test-suite? test test-plan)
+               (not (::no-instrument test)))
+      (instrument))
     test)
 
   (post-test [test test-plan]
-    (when (test-suite? test)
+    (when (test-suite? test test-plan)
       (unstrument))
     test))
